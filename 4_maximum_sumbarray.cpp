de092c552aede9ce38e050380c1fab20af24935f
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-06-14 00:50:15 +0530

    Add files via upload

---
+++ 4_maximum_sumbarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+//kadane's algorithm
+    int maxSubArray(vector<int>& nums) {
+         int n= nums.size();
+        int sum=0, maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            sum+= nums[i];
+            maxi= max(sum, maxi);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+    }
+};
