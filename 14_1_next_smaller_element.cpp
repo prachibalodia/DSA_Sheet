79b3578b516302bf647c37c0bfdde9316788e38c
Prachi Balodia <89089608+prachibalodia@users.noreply.github.com>
2023-07-26 21:34:07 +0530

    Add files via upload

---
+++ 14_1_next_smaller_element.cpp
@@ -0,0 +1,16 @@
+void immediateSmaller(vector<int>& a)
+{
+	// Write your code here.
+	//T= O(N), S=O(1)
+	vector<int>v;
+	int n= a.size();
+	for(int i=0;i<n-1;i++){
+		if(a[i] > a[i+1]){
+			a[i]=a[i+1];
+		}
+		else{
+			a[i]= -1;
+		}
+	}
+	a[n-1]=-1;
+}
